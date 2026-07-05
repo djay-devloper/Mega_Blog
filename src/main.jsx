69d12d03d9346db9107ca181c79f1ccfcb84c7d7
Dhananjay Yadav <djyadav639@Mac.lan>
2026-07-05 09:57:24 +0530

    deployment correction

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
-import AllPosts from "./pages/AllPosts";
+import AllPosts from "./pages/Allposts";
 
 
 const router = createBrowserRouter([
