d765fff0fa8b20a753c69273617bfb3afc01cabb
Dhananjay Yadav <djyadav639@Mac.lan>
2026-07-05 10:12:48 +0530

    deploying..

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
 import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
+import LogoutBtn from "./header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Button from "./Button";
+import Button from "./button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import Input from "./input";
 export {
     Header,
     Footer,
